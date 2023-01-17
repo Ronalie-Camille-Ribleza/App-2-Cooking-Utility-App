9d2bcb1d382bb6814a10a73f23d23d1f0415fe45
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2023-01-17 22:08:16 +0400

    Added modal

--- app/src/main/java/com/example/cookingutilityapp/MainActivity.kt
+++ app/src/main/java/com/example/cookingutilityapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.cookingutilityapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.widget.Button
 
@@ -16,7 +19,22 @@ class MainActivity : AppCompatActivity() {
         ingredientButton = findViewById(R.id.ingredientButton)
         equipmentButton = findViewById(R.id.equipmentButton)
         timerButton = findViewById(R.id.timerButton)
+        val about = findViewById<Button>(R.id.helpButton)
 
+        // modal for app tutorial
+        about.setOnClickListener{ val modalBinding = layoutInflater.inflate(R.layout.activity_modal, null)
+            val aboutModal = Dialog(this)
+            aboutModal.setContentView(modalBinding)
+
+            aboutModal.setCancelable(true)
+            aboutModal.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            aboutModal.show()
+
+            val start = aboutModal.findViewById<Button>(R.id.startApp)
+            start.setOnClickListener {
+                aboutModal.dismiss()
+            }
+        }
         ingredientButton.setOnClickListener {
             val goToIngredients = Intent(this, Ingredients::class.java)
             startActivity(goToIngredients)
